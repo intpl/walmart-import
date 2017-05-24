952e0faa14f151bbe1f1defd20faae8fc7621bd9
Bartek Gladecki <bgladecki@gmail.com>
2017-05-24 18:33:52 +0200

    job for importing product

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,9 +1,11 @@
 class MainController < ApplicationController
   def index
+    @products = Product.all
   end
 
   def submit
-    flash['notice'] = params[:url]
+    ScrapeProductJob.new.perform(params[:url])
+    flash['notice'] = 'started importing product background job...'
     redirect_to root_path
   end
 end
---
+++ app/jobs/scrape_product_job.rb
@@ -0,0 +1,31 @@
+class ScrapeProductJob < ApplicationJob
+  # queue_as :default
+
+  def perform(url)
+    @url = url
+    walmart_id or raise 'invalid url'
+
+    @doc = Nokogiri::HTML(open(@url))
+
+    Product.create(
+      walmart_id: walmart_id,
+      name: name,
+      price: price
+    )
+  end
+
+  def name
+    @doc.at_css(".prod-ProductTitle > div").text
+  end
+
+  def price
+    price_c = @doc.at_css('.Price-characteristic').text
+    price_m = @doc.at_css('.Price-mantissa').text
+
+    "#{price_c}#{price_m}".to_i
+  end
+
+  def walmart_id
+    @walmart_id ||= @url.match(/\A(https*:\/\/)?(www\.)?walmart.com\/ip\/[\w-]*\/(\d+)/).try(:[], 3)
+  end
+end
